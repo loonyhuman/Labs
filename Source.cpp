e674704e8225b4445577e08dfb9d1475559b0e27
Илья <33789045+loonyhuman@users.noreply.github.com>
2017-12-02 19:59:56 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,19 @@
 #include <ctime>
 #define M 3
 #define N 3
-#define k 0
 using namespace std;
+
+int** allocateMemory(int m, int n)
+{
+	int** pointer = new int*[m];
+
+	for (int i = 0; i < m; ++i)
+	{
+		pointer[i] = new int[n];
+	}
+
+	return pointer;
+}
 
 int random(int min, int max)
 {
@@ -12,6 +23,7 @@ int random(int min, int max)
 
 void fill(int** matrix, int m, int n)
 {
+	srand(time(NULL));
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -32,22 +44,26 @@ void print(int** matrix, int m, int n)
 	}
 }
 
+int solution(int** matrix, int m, int n, int q)
+{
+	int l = 0;
+	for (int i = 0; i < M; i++)
+		for (int j = 0; j<N; j++)
+			if (matrix[i][j]<0 && abs(matrix[i][j]) > q)
+				l = l + 1;
+	return l;
+}
+
 int main()
 {
-	int** A = new int*[M];
-	for (int i = 0; i < M; ++i)
-		A[i] = new int[N];
-
-	srand(time(NULL));
+	int** A;
+	A = allocateMemory(M, N);
 	fill(A, M, N);
 	print(A, M, N);
-	int i, j;
-	int n = 0;
-		for (i = 0; i < M; i++)
-			for (j = 0; j<N; j++)
-				if (A[i][j]<0 && abs(A[i][j]) > k)
-					n = n + 1;
-		cout << n << endl;
+	cout << "Input k: " << endl;
+	int k;
+	cin >> k;
+	cout << "n= " << solution(A, M, N, k) << endl;
 	system("pause");
 	return 0;
 }
